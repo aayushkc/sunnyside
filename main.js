c5639d0789e2d4d13f90b3c85b3e8415fef76cb7
aayushkc <57221458+aayushkc@users.noreply.github.com>
2022-02-05 23:15:19 +0545

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+let ham = document.querySelector('.ham')
+let menu = document.querySelector('.menu')
+let menuBar = document.querySelector('.menu-bar')
+
+
+ham.addEventListener('click', function(){
+	menuBar.classList.toggle('menu')
+	
+})
